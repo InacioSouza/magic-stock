3fa2bf9eb601a5628c4860035e0d0bad60be76cb
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2026-04-28 09:45:54 -0300

    front - feat: Interceptors adicionados para verificar usuário logado e adicionar token as requisições

--- front/src/core/api.ts
+++ front/src/core/api.ts
@@ -2,4 +2,29 @@ import axios from 'axios'
 
 export const api = axios.create({
     baseURL: 'http://localhost:3000'
-})
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access_token");
+
+        if (token && config.url !== '/auth/login') {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if(error.response && error.response.status === 401) {
+            localStorage.removeItem("access_token");
+            window.location.href = "/login";
+        }
+
+        return Promise.reject(error);
+    }
+)
