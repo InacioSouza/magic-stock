7ec9191b78bc1edec53a80003050cb73200d778e
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2026-05-06 15:47:19 -0300

    front - ui: Componente SelectEntity implementado

---
+++ front/src/core/components/SelectEntity/SelectEntity.tsx
@@ -0,0 +1,51 @@
+
+import { useState } from 'react';
+import styles from './SelectEntity.module.css';
+
+type SelectEntityProps = {
+    label?: string,
+    startEntityID?: number;
+    listEntity: any[];
+    displayField: string;
+    onChange: (entity: any) => void
+}
+
+const SelectEntity = ({
+    label,
+    startEntityID,
+    displayField,
+    listEntity,
+    onChange
+}: SelectEntityProps) => {
+
+    const [value, setValue] = useState(startEntityID);
+
+    return (
+        <div className={styles.selectEntity}>
+            {
+                label ?
+                    <label htmlFor='selectEntityID'>{`${label}: `}</label>
+                    : ''
+            }
+            <select
+                id='selectEntityID'
+                value={value}
+                onChange={(event) => {
+                    setValue(Number(event.target.value))
+                    onChange(event.target.value);
+                }}
+            >
+                {listEntity && listEntity.map(entity => {
+                    return <option
+                        value={entity.id}
+                        key={entity.id}
+                    >
+                        {entity[displayField]}
+                    </option>
+                })}
+            </select>
+        </div>
+    );
+}
+
+export default SelectEntity;
