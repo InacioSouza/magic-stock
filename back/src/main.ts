28bbaae905e55b9e62d9d08cde29bba6d1394251
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2026-05-12 11:17:52 -0300

    back - refactor: Política de CORS atualizada

--- back/src/main.ts
+++ back/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   const corsOptions = {
     origin: ["http://localhost:5173",],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
   };
